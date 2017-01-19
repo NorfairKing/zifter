7d3dc3e7f0e0cacda43af2782bcaad7b1c4af171
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-01-19 14:08:36 +0100

    Upgraded zifter-stack to be non-zifter-specific

--- zifter-stack/src/Zifter/Stack.hs
+++ zifter-stack/src/Zifter/Stack.hs
@@ -2,9 +2,12 @@
 
 module Zifter.Stack where
 
+import Control.Monad
 import Control.Monad.IO.Class
+import Data.List
 import System.Exit (ExitCode(..))
-import System.Process (system)
+import System.Process
+       (system, shell, readCreateProcessWithExitCode)
 
 import Zifter.Zift
 
@@ -21,17 +24,33 @@ stackCheckAndPrintVersion = do
         ExitFailure c -> fail $ unwords [cmd, "failed with exit code", show c]
         ExitSuccess -> pure ()
 
+stackGetPackageTargetTuples :: Zift [(String, [String])]
+stackGetPackageTargetTuples = do
+    ((_, _, psstr), (_, _, tsstr)) <-
+        (,) <$>
+        liftIO (readCreateProcessWithExitCode (shell "stack ide packages") "") <*>
+        liftIO (readCreateProcessWithExitCode (shell "stack ide targets") "")
+    pure $ matchUpPackagesAndTargets (lines psstr) (lines tsstr)
+
+matchUpPackagesAndTargets :: [String] -> [String] -> [(String, [String])]
+matchUpPackagesAndTargets packages targets =
+    flip map packages $ \package ->
+        (package, filter ((package ++ ":") `isPrefixOf`) targets)
+
 stackBuild :: Zift ()
 stackBuild = do
-    let cleanCmd = "stack clean zifter zifter-cabal zifter-hindent zifter-stack" -- TODO read the cabal file to find the target.
-    cec <- liftIO $ system cleanCmd
-    case cec of
-        ExitFailure c ->
-            fail $ unwords [cleanCmd, "failed with exit code", show c]
-        ExitSuccess -> pure ()
-    let buildCmd = "stack build --pedantic"
-    bec <- liftIO $ system buildCmd
-    case bec of
-        ExitFailure c ->
-            fail $ unwords [buildCmd, "failed with exit code", show c]
-        ExitSuccess -> pure ()
+    tups <- stackGetPackageTargetTuples
+    forM_ tups $ \(package, targets) -> do
+        let cleanCmd = "stack clean " ++ package
+        cec <- liftIO $ system cleanCmd
+        case cec of
+            ExitFailure c ->
+                fail $ unwords [cleanCmd, "failed with exit code", show c]
+            ExitSuccess -> pure ()
+        forM_ targets $ \target -> do
+            let buildCmd = "stack build --pedantic " ++ target
+            bec <- liftIO $ system buildCmd
+            case bec of
+                ExitFailure c ->
+                    fail $ unwords [buildCmd, "failed with exit code", show c]
+                ExitSuccess -> pure ()
